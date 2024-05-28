a29164d5076884a9a5adc90efe61e1606b02ff0c
05tushar1997 <161101934+05tushar1997@users.noreply.github.com>
2024-05-28 16:26:51 +0530

    Add files via upload

---
+++ logistic.sql
@@ -0,0 +1,8 @@
+create database logistic
+use logistic
+select * from deliveries
+select * from drivers
+select * from fleets
+select * from operational_costs
+select * from shipments
+select * from status
